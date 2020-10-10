434ae15f829a98f95db43a6f68e3cdb8d2764e78
alexnicholls1999 <azini@live.co.uk>
2020-10-10 19:14:18 +0100

    add slider

--- main.js
+++ main.js
@@ -14,3 +14,38 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+// slider
+
+const next = document.querySelector('.next');
+const previous = document.querySelector('.previous');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+display(index);
+function display (index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide () {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+
+
+function previousSlide () {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+next.addEventListener('click', nextSlide);
+previous.addEventListener('click', previousSlide);
