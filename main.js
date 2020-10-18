7df79e2bc7b09a11b9459649f075ad50b947b773
alexnicholls1999 <azini@live.co.uk>
2020-10-18 14:05:20 +0100

    add changes

--- main.js
+++ main.js
@@ -20,24 +20,35 @@ menuBtn.addEventListener('click', () => {
 const next = document.querySelector('.next');
 const previous = document.querySelector('.previous');
 const slides = document.querySelectorAll('.slide');
+const image = document.getElementById('slide');
 
 let index = 0;
 display(index);
-function display (index) {
+function display(index, slide) {
     slides.forEach((slide) => {
-        slide.style.display = 'none';
+        slide.style.display = 'none'; 
     });
     slides[index].style.display = 'flex';
-
+    
 }
 
-function nextSlide () {
+function fadeIn(el){
+    slides.classList.add('show');
+    slides.classList.remove('hide');  
+}
+  
+function fadeOut(el){
+    slides.classList.add('hide');
+    slides.classList.remove('show');
+}
+
+function nextSlide() {
     index++;
     if (index > slides.length - 1) {
         index = 0;
-    }
+    } 
+    fadeIn();
     display(index);
-    slides.classList.toggle('fade');
 }
 
 
@@ -46,6 +57,7 @@ function previousSlide () {
     if (index < 0) {
         index = slides.length - 1;
     }
+    fadeOut();
     display(index);
 }
 
