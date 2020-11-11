3da4db52efcf4f1383b9ea42f92ff7ec1b1df7be
alexnicholls1999 <azini@live.co.uk>
2020-11-11 19:25:12 +0000

    added animations and simplified homepage for public use

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const menuBtn = document.querySelector('.hamburger');
-const nav = document.querySelector('.navbar');
+// const menuBtn = document.querySelector('.hamburger');
+// const nav = document.querySelector('.navbar');
 
-let menuOpen = false;
+// let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen){
-        menuBtn.classList.add('open');
-        nav.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        nav.classList.remove('open');
-        menuOpen = false;
-    }
-});
+// menuBtn.addEventListener('click', () => {
+//     if(!menuOpen){
+//         menuBtn.classList.add('open');
+//         nav.classList.add('open');
+//         menuOpen = true;
+//     } else {
+//         menuBtn.classList.remove('open');
+//         nav.classList.remove('open');
+//         menuOpen = false;
+//     }
+// });
 
 // slider
 
