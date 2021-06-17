e5587e926ae9ad405404e8e0c8091ee6bef5adfb
alexnicholls1999 <azini@live.co.uk>
2021-06-17 21:58:39 +0100

    add search hook

---
+++ src/react-hooks/useSearch.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import { db } from "./../firebase/utils";
+
+export default function useSearch() {
+    const [updateEvents, setUpdateEvents] = useState([]);
+    const [state, setState] = useState({
+        date: "Any",
+        time: "Any",
+        type: "Any",
+        age: "Any"
+    })
+
+
+    const handleOnChangeSearch = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleOnChangeAvailableSlots = (e) => {
+
+        setState({
+            date: state.date,
+            time: state.time,
+            type: state.time,
+            age: state.age,
+            isChecked: e.target.checked,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(state);
+
+        let ref = db.collection("events")
+
+        if (state.isChecked === true) {
+            ref = ref.where("slots", "!=", 0)
+        }
+
+
+        if (state.date !== "Any") {
+            ref = ref.where("date", "==", state.date)
+        } else {
+            ref = ref.where("datefltr", "==", true)
+        }
+
+        if (state.type !== "Any") {
+            ref = ref.where("eventsType", "==", state.type)
+        } else {
+            ref = ref.where("eventsTypefltr", "==", true) 
+        }
+    
+        if (state.type !== "Any") {
+        ref = ref.where("eventsType", "==", state.type)
+        } else {
+        ref = ref.where("eventsTypefltr", "==", true)
+        }
+    
+        if (state.time !== "Any") {
+        ref = ref.where("time", "==", state.time)
+        } else {
+        ref = ref.where("timefltr", "==", true)
+        }
+    
+        if (state.age !== "Any") {
+        ref = ref.where("age", "==", state.age)
+        } else {
+        ref = ref.where("agefltr", "==", true)
+        }
+
+        ref.onSnapshot((snapshot) => {
+            const newEvents = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+      
+            }));
+      
+            setUpdateEvents(newEvents);
+          }); 
+
+    }
+    
+
+
+    useEffect(() => {
+        db.collection("events").orderBy("date").onSnapshot((snapshot) => {
+        const newEvents = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+    
+        setUpdateEvents(newEvents);
+        });
+
+    }, []);
+      
+    
+    return {
+        updateEvents,
+        state,
+        handleOnChangeAvailableSlots,
+        handleOnChangeSearch,
+        handleSubmit
+    }
+
+}
