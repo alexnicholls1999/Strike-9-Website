dc5da0ca8a505856af61879d2d41cb2af304d3e5
alexnicholls1999 <azini@live.co.uk>
2021-08-31 16:06:01 +0100

    add changes to About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,22 @@
+import styled from "styled-components";
+import Button from "../Components/Atoms/Buttons/Button";
+import Hero from "../Components/Organisms/Hero";
+
+const ButtonWrapper = styled.div`
+    width: 250px;
+`;
+
+
 function About() {
     return (
-        <div>
-            About Page
-        </div>
+        <>
+            <Hero secondary heroDetails={{title: "ABOUT STRIKE 9 TRAINING", paragraph: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt recusandae eos tempora harum cupiditate eveniet vitae optio vero facilis itaque."}} >
+                <ButtonWrapper>
+                    <Button text="REGISTER" />
+                </ButtonWrapper>
+            </Hero>
+
+        </>
     )
 }
 
