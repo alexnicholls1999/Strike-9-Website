b759aafb9c6f827c51604bc3fdf13d49bbd9a3fd
alexnicholls1999 <azini@live.co.uk>
2021-11-05 15:29:50 +0000

    added useAuth hook

---
+++ src/react-hooks/useAuth.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function useAuth(firebaseAuth) {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [ user, setUser ] = useState([]);
+
+    const googleProvider = new firebaseAuth.GoogleAuthProvider();
+    const facebookProvider = new firebaseAuth.FacebookAuthProvider();
+    const twitterProvider = new firebaseAuth.TwitterAuthProvider();
+
+    firebaseAuth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            setIsAuthenticated(true);
+            setUser(firebaseUser);
+            return;
+        }
+
+        setIsAuthenticated(false)
+    });
+
+    const createEmailUser = (email, password) => firebaseAuth().createUserWithEmailAndPassword(email, password);
+
+    const signInEmailUser = (email, password) => firebaseAuth().signInEmailUser(email, password);
+
+    const signInWithThirdPartyProvider = (provider) => {
+        switch (provider) {
+            case "google":
+                firebaseAuth().signInWithRedirect(googleProvider);
+            case "facebook": 
+                firebaseAuth().signInWithRedirect(facebookProvider);
+            case "twitter": 
+                firebaseAuth().signInWithRedirect(twitterProvider);
+            default:
+                throw new Error("unsupported provider!")
+        }
+    } 
+
+    const signOut = () => firebaseAuth().signOut();
+
+    return {
+        isAuthenticated,
+        user,
+        createEmailUser,
+        signInEmailUser,
+        signOut
+    }
+}
